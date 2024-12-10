5b79455885b9311a5beb10b4523f7c068e270b56
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-12-10 15:01:39 +0700

    refactor: Move routes to different file

---
+++ config/routes.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+use Slim\Routing\RouteCollectorProxy;
+use \App\Controllers\ApiRoot;
+use App\Controllers\Account;
+use App\Controllers\PlayerData;
+use App\Middleware\ValidateId;
+use App\Middleware\AddJsonResponseHeader;
+
+// Route for the root of the API
+$app->group('/api', function (RouteCollectorProxy $group) {
+    $group->get('/', ApiRoot::class); // Display a welcome message
+
+    // Account routes
+    $group->group('/account', function (RouteCollectorProxy $group){
+        // General account actions
+        $group->get('/', [Account::class, 'getAll']); // Get all accounts
+        $group->post('/', [Account::class, 'create']); // Create a new account
+        $group->put('/login', [Account::class, 'login']); // Login
+
+        // Actions that require a specific account ID
+        $group->group('/{id:[0-9+]}', function (RouteCollectorProxy $group){
+            $group->get('/', [Account::class, 'getById']); // Get target account info
+            $group->patch('/', [Account::class, 'updatePassword']); // Update target account password
+        })->add(ValidateId::class); // Validate the account ID
+    });
+
+    // Player data routes
+    $group->group('/player-data', function (RouteCollectorProxy $group){
+        // General player data actions
+        $group->get('/', [PlayerData::class, 'getAll']);
+
+        // Actions that require a specific account ID
+        $group->group('/{id:[0-9]+}', function (RouteCollectorProxy $group){
+            $group->get('/', [PlayerData::class, 'getById']); // Get target player data
+            $group->patch('/', [PlayerData::class, 'updateData']); // Update target player data
+        })->add(ValidateId::class); // Validate the account ID
+    });
+})->add(AddJsonResponseHeader::class); // Add JSON response header to all routes in the group
--- public/index.php
+++ public/index.php
@@ -5,11 +5,6 @@ declare(strict_types=1);
 use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
 use Slim\Handlers\Strategies\RequestResponseArgs;
-use Slim\Routing\RouteCollectorProxy;
-use App\Controllers\Account;
-use App\Controllers\PlayerData;
-use App\Middleware\AddJsonResponseHeader;
-use App\Middleware\ValidateId;
 
 define('APP_ROOT', dirname(__DIR__));
 
@@ -36,49 +31,14 @@ $app = AppFactory::create();
 $app->getRouteCollector()->setDefaultInvocationStrategy(new RequestResponseArgs());
 
 // Add middleware
-$app->add(new AddJsonResponseHeader()); // Add JSON response header to all responses
 $app->addBodyParsingMiddleware(); // Add body parsing middleware
-$error_middleware = $app->addErrorMiddleware(displayErrorDetails: true,
-                                             logErrors: true, 
-                                             logErrorDetails: true); // Add error handling middleware
+$app->addErrorMiddleware(displayErrorDetails: true,
+                         logErrors: true, 
+                         logErrorDetails: true); // Add error handling middleware
 
 
-// Configure the default error handler to return JSON
-$error_handler = $error_middleware->getDefaultErrorHandler();
-$error_handler->forceContentType('application/json'); // Force JSON error responses
-
-// Account routes
-$app->group('/account', function (RouteCollectorProxy $group){
-    
-    $group->get('', [Account::class, 'getAll']);
-
-    $group->post('', [Account::class, 'create']);
-
-    $group->put('/login', [Account::class, 'login']);
-
-    $group->group('/{id:[0-9+]}', function (RouteCollectorProxy $group){
-        
-        $group->get('', [Account::class, 'getById']);
-
-        $group->patch('', [Account::class, 'updatePassword']);
-
-    })->add(ValidateId::class);
-
-});
-
-// Player data routes
-$app->group('/player-data', function (RouteCollectorProxy $group){
-    $group->get('', [PlayerData::class, 'getAll']);
-    
-    $group->group('/{id:[0-9]+}', function (RouteCollectorProxy $group){
-        
-        $group->get('', [PlayerData::class, 'getById']);
-
-        $group->patch('', [PlayerData::class, 'updateData']);
-
-    })->add(ValidateId::class);;
-
-});
+// Load the routes
+require_once APP_ROOT . '\config\routes.php';
 
 // Run the Slim app
 $app->run();
