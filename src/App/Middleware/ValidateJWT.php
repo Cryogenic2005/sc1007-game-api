45cef65eb01734bc91627aaea15099f353b93cbc
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-12-23 07:13:35 +0800

    Check correct token type before verifying as valid. Minor refactoring

--- src/App/Middleware/ValidateJWT.php
+++ src/App/Middleware/ValidateJWT.php
@@ -4,21 +4,18 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
-use App\Repositories\UserRepository;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Psr7\Factory\ResponseFactory;
 use App\Authorization\JWTHelper;
 use App\Authorization\JWTError;
+use App\Authorization\JWTType;
 
 /** Middleware for validating a JWT */
 class ValidateJWT
 {
-    public function __construct(private ResponseFactory $responseFactory,
-                                private UserRepository $userRepository)
-    {   
-    }
+    public function __construct(private ResponseFactory $responseFactory) { }
 
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
@@ -40,10 +37,18 @@ class ValidateJWT
             return $response->withStatus(400);
         }
 
-        $payload = JWTHelper::getJWTPayload($jwt, $_ENV['JWT_SECRET']);
+        $payload = JWTHelper::getJWTPayload($jwt);
 
         if ($payload['status'] === JWTError::SUCCESS)
         {
+            if ($payload['token_type'] !== JWTType::ACCESS->value) {
+                $response = $this->responseFactory->createResponse();
+
+                $response->getBody()->write(json_encode(['error' => 'Invalid JWT type']));
+                
+                return $response->withStatus(401);
+            }
+
             $request = $request
                 ->withAttribute('sub', $payload['sub'])
                 ->withAttribute('name', $payload['name']);
@@ -52,16 +57,16 @@ class ValidateJWT
         
         $response = $this->responseFactory->createResponse();
         $error_message = match($payload['status']) {
-            JWTError::EXPIRED => 'JWT expired',
-            JWTError::BEFORE_VALID => 'JWT not yet valid',
+            JWTError::EXPIRED           => 'JWT expired',
+            JWTError::BEFORE_VALID      => 'JWT not yet valid',
             JWTError::SIGNATURE_INVALID => 'Invalid JWT signature',
-            JWTError::JWT_EXCEPTION => 'Error decoding JWT',
-            JWTError::UNKNOWN => 'Unknown error',
+            JWTError::JWT_EXCEPTION     => 'Error decoding JWT',
+            JWTError::UNKNOWN           => 'Unknown error',
         };
         $error_code = match($payload['status']) {
             JWTError::JWT_EXCEPTION => 400,
-            JWTError::UNKNOWN => 500,
-            default => 401,
+            JWTError::UNKNOWN       => 500,
+            default                 => 401,
         };
 
         $response->getBody()->write(json_encode(['error' => $error_message]));
