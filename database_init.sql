1e5ebf126638220cb1d393a4ca654c7883b70681
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2025-01-08 19:09:22 +0700

    Upload database initialisation file

---
+++ database_init.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE IF NOT EXISTS `sc1007_db`;
+
+USE `sc1007_db`;
+
+--
+-- Create tables for the database
+--
+
+CREATE TABLE IF NOT EXISTS `account_info` (
+  `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL COMMENT 'Hashed Password',
+  `isLogged` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `refresh_tokens` (
+  `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `token` char(64) NOT NULL COMMENT 'SHA-256 Hashed Refresh Token',
+  `user_id` mediumint(8) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `user_id` (`user_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `account_info`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Create users for the database
+--
+
+CREATE USER 'account_info_user'@'localhost' IDENTIFIED BY 'PLACEHOLDER_PASSWORD';
+CREATE USER 'refresh_tokens_user'@'localhost' IDENTIFIED BY 'PLACEHOLDER_PASSWORD';
+
+--
+-- Grant permissions to the users
+--
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON `sc1007_db`.`account_info` TO 'account_info_user'@'localhost';
+GRANT SELECT, INSERT, DELETE ON `sc1007_db`.`refresh_tokens` TO 'refresh_tokens_user'@'localhost';
+
+FLUSH PRIVILEGES;
