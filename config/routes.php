778a0cfd9608d0b80728c3646bb2bd07c2a7910e
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2025-01-14 21:30:09 +0700

    Update routes

--- config/routes.php
+++ config/routes.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 use Slim\Routing\RouteCollectorProxy;
 
-use App\Controllers\Home; // Controller for the home page
-use App\Controllers\ApiRoot; // Controller for the root of the API
-use App\Controllers\Login; // Controller for login
-use App\Controllers\TokenIssuer; // Controller for managing JSON Web Token
-use App\Controllers\Account; // Controller for managing actions on accounts
+use App\Controllers\Account;            // Controller for managing actions on accounts
+use App\Controllers\ApiRoot;            // Controller for the root of the API
+use App\Controllers\CodeSubmission;     // Controller for submitting code to be executed
+use App\Controllers\Login;              // Controller for login
+use App\Controllers\TokenIssuer;        // Controller for managing JSON Web Token
 
-use App\Middleware\AddJsonResponseHeader; // Middleware for adding JSON response header to responses
-use App\Middleware\ValidateId; // Middleware for validating an ID
-use App\Middleware\ValidateJWT; // Middleware for validating a JWT
-
-// Route to home page
-$app->get('/', Home::class); // Display the home page (Use this route to test if the API is working)
+use App\Middleware\AddJsonResponseHeader;   // Middleware for adding JSON response header to responses
+use App\Middleware\ValidateId;              // Middleware for validating an ID
+use App\Middleware\ValidateJWT;             // Middleware for validating a JWT
 
 // Route for the root of the API
 $app->group('/api', function (RouteCollectorProxy $group) {
@@ -24,24 +21,30 @@ $app->group('/api', function (RouteCollectorProxy $group) {
 
     $group->post('/login', Login::class); // Login to the API
 
-    // Routes for retrieve JWTokens
     $group->post('/token', TokenIssuer::class); // Retrieve a new JWT
 
-    // Account routes
-    $group->group('/account', function (RouteCollectorProxy $group){
+    $group->group('', function (RouteCollectorProxy $group) {
+    
+        // Route for submitting code to be executed
+        $group->post('/code', CodeSubmission::class);
 
-        $group->get('', [Account::class, 'getAll']); // Get all accounts
-
-        $group->post('', [Account::class, 'create']); // Create new account
-
-        // Actions that require a specific account ID
-        $group->group('/{id:[0-9+]}', function (RouteCollectorProxy $group){
-
-            $group->get('', [Account::class, 'getById']); // Get account info
-
-            $group->patch('', [Account::class, 'updatePassword']); // Update account password
-
-        })->add(ValidateId::class); // Validate the account ID
+        // Account routes
+        $group->group('/account', function (RouteCollectorProxy $group){
+    
+            $group->get('', [Account::class, 'getAll']); // Get all accounts
+    
+            $group->post('', [Account::class, 'create']); // Create new account
+    
+            // Actions that require a specific account ID
+            $group->group('/{id:[0-9+]}', function (RouteCollectorProxy $group){
+    
+                $group->get('', [Account::class, 'getById']); // Get account info
+    
+                $group->patch('', [Account::class, 'updatePassword']); // Update account password
+    
+            })->add(ValidateId::class); // Validate the account ID
+    
+        });
 
     })->add(ValidateJWT::class); // Require a valid JWToken for all routes in this group
 
