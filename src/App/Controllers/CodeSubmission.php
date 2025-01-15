6a0797df213b7d210b0513884033eaeae7bca25c
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2025-01-15 19:30:17 +0700

    Rework code submission logic
    - Can accept input text which is passed to the program
    - Refactoring

--- src/App/Controllers/CodeSubmission.php
+++ src/App/Controllers/CodeSubmission.php
@@ -29,7 +29,8 @@ class CodeSubmission
 
         $this->codeValidator = new Validator();
         $this->codeValidator->mapFieldsRules([
-            'code' => ['required']
+            'code' => ['required'],
+            'input' => ['required'],
         ]);
     }
 
@@ -46,45 +47,31 @@ class CodeSubmission
 
             return $response->withStatus(422);
         }
-
-        $code = $body['code'];
         
-        // Get the current time, saves as a unique identifier for the request
-        $request_id = uniqid();
-
-        // Create a temporary file to store the code
-        $tempFile = $this->createTempfile($code, $request_id);
-
         // Create a Docker container worker to run the code
-        $container_name = $this->createWorker($request_id);
+        $container_name = $this->createWorker();
 
-        // Copy the code to the Docker container
-        $copy_command = sprintf(
-            "docker cp %s %s:/home/restricted/code/user_code.py",
-            escapeshellarg($tempFile),
-            escapeshellarg($container_name)
-        );
-        shell_exec($copy_command);
+        $this->writeToContainerFile($body['code'], 'user_code.py', $container_name);
+        $this->writeToContainerFile($body['input'], 'input.txt', $container_name);
 
-        // Run the code in the Docker container
-        $json_output = $this->executeCode($container_name);
-
-        // Cleanup
-        $this->cleanup($tempFile, $container_name);
+        try{ // Run the user code in the Docker container
+            $json_output = $this->executeCode($container_name);
+        }
+        catch(\Exception $e){ // Shouldn't happen, but just in case
+            $json_output = [
+                'status' => 'error',
+                'stderr' => $e->getMessage(),
+            ];
+        }
+        finally{ // Remove the Docker container
+            shell_exec("docker rm -f $container_name");
+        }
 
         $response->getBody()->write(json_encode($json_output, JSON_FORCE_OBJECT));
         return $response;
     }
 
-    private function createTempfile(string $code, string $request_id): string
-    {
-        $tempFile = tempnam(sys_get_temp_dir(), "script{$request_id}_");
-        file_put_contents($tempFile, $code);
-
-        return $tempFile;
-    }
-
-    private function createWorker(string $request_id): string
+    private function createWorker(): string
     {
         // Build the Docker image
         $build_command = sprintf(
@@ -92,12 +79,11 @@ class CodeSubmission
             escapeshellarg(self::$DOCKERFILE_PATH),
             escapeshellarg(self::$DOCKER_PATH)
         );
-
+        
         shell_exec($build_command);
         
         // Run the Docker container
-        $container_name = "python-restricted-container-{$request_id}";
-        
+        $container_name = uniqid('container-', true);
         $docker_run_command = "docker run -itd --name " . escapeshellarg($container_name)
             . " -m 128m" // Limit memory usage to 128MB
             . " python-restricted-image"; // Use the Docker image
@@ -105,6 +91,29 @@ class CodeSubmission
         shell_exec($docker_run_command);
 
         return $container_name;
+    }
+
+    private function writeToContainerFile(array|string $data, string $filename, string $container_name)
+    {
+        // If data is a list, convert it to a string
+        // by joining the elements with a newline character
+        if(is_array($data)){
+            $data = implode("\n", $data);
+        }
+
+        $tempFile = tempnam(sys_get_temp_dir(), '');
+        file_put_contents($tempFile, $data);
+
+        // Copy the code to the Docker container
+        shell_exec(sprintf(
+            "docker exec -i %s tee %s < %s",
+            escapeshellarg($container_name),
+            escapeshellarg($filename),
+            escapeshellarg($tempFile)
+        ));
+
+        // Remove the temporary file
+        unlink($tempFile);
     }
 
     private function executeCode(string $container_name)
@@ -119,24 +128,25 @@ class CodeSubmission
         $tempErrorFile = tempnam(sys_get_temp_dir(), 'stderr');
         $tempOutputFile = tempnam(sys_get_temp_dir(), 'stdout');
 
-        try{
-            // Execute the code in the Docker container, and redirect stderr to a file
+        try{ 
             $execute_code_command = sprintf(
-                "docker exec %s timeout %d python3 /home/restricted/code/user_code.py",
+                "docker exec %s timeout %d python3 execute_user_code.py",
                 escapeshellarg($container_name),
                 self::$TIMEOUT
-            );
-            $route_std_command = " >$tempOutputFile 2>$tempErrorFile";
+            ); // Execute the code in the Docker container and limit the execution time
+            
+            $route_std_command = sprintf(
+                " > %s 2> %s",
+                escapeshellarg($tempOutputFile),
+                escapeshellarg($tempErrorFile)
+            ); // Route the stdout and stderr to temporary files
 
             exec($execute_code_command . $route_std_command, result_code: $return_code);
 
-            // Read the stdout file
+            // Read the stdout and stderr from the temporary files
             $output = file_get_contents($tempOutputFile, length: self::$MAX_LENGTH);
-
-            // Read the stderr file
             $stderr = file_get_contents($tempErrorFile, length: self::$MAX_LENGTH);
 
-            // Get the output of the code execution
             $json_output['status'] = $return_code === 0 ? 'success' : 'error';
             $json_output['stdout'] = $output;
             $json_output['stderr'] = $return_code === 124 ? 'Timed out' : $stderr;
@@ -152,14 +162,5 @@ class CodeSubmission
         }
 
         return $json_output;
-    }
-
-    private function cleanup(string $tempFile, string $container_name)
-    {
-        // Remove the temporary file
-        unlink($tempFile);
-
-        // Remove the Docker container
-        shell_exec("docker rm -f $container_name");
     }
 }
